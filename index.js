34f67266e46edfe02787ef8d7b99f4ff03bfd713
aramanrajput <armanrajput992@gmail.com>
2022-11-15 12:37:42 +0530

    vercel config added

--- index.js
+++ index.js
@@ -8,19 +8,15 @@ let app = express()
 
 app.use(express.json())
 
+let connectdb = ()=>connect("mongodb://127.0.0.1:27017/fargo")
 
-
-  connect("mongodb://127.0.0.1:27017/fargo",{useNewUrlParser:true,
-  useUnifiedTopology:true
- },(err) => {
-      if(err) {console.log(err) }
-      else {console.log("mongdb is connected")}})
+ 
   let Port = process.env.PORT || 8080
   
   
   app.listen(Port,async()=>{
      console.log("server is running on 8080")
-     
+     await connectdb()
   })
 
   let userController=require("./src/controller/user.controller")
