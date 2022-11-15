b2d50aa34394120f813c5fbf3802f7f49be8e71e
aramanrajput <armanrajput992@gmail.com>
2022-11-15 13:24:00 +0530

    fixing deployment

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ let app = express()
 
 app.use(express.json())
 
-let connectdb = ()=>connect("mongodb://127.0.0.1:27017/fargo")
+let connectdb = ()=>connect("mongodb+srv://arman992:arman992@cluster0.y1bch.mongodb.net/fargo")
 
  
   let Port = process.env.PORT || 8080
--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -4,16 +4,16 @@ let User = require("../model/user.model")
 
 
 
-// router.post("/",async(req,res)=>{
-//     try{
-//         let user = await User.create(req.body)
-//         res.send({user})
-//     }catch(e){
-// res.send({error:true,message:e.message})
-//     }
+router.post("/",async(req,res)=>{
+    try{
+        let user = await User.create(req.body)
+        res.send({user})
+    }catch(e){
+res.send({error:true,message:e.message})
+    }
 
 
-// })
+})
 router.get("/",async(req,res)=>{
     try{
         let user = await User.find()
